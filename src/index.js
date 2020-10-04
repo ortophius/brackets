a571f359200af2b7642e6c64749e808d319041ef
ortophius@mail.ru <ortophius@mail.ru>
2020-10-04 23:22:31 +0400

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openingBrackets = bracketsConfig.map(brackets => brackets[0]).join('');
+  const closingBrackets = bracketsConfig.map(brackets => brackets[1]).join('');
+  let expectedBrackets = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const isOpeningBracket = openingBrackets.indexOf(char) !== -1;
+    const isClosingBracket = closingBrackets.indexOf(char) !== -1;
+    const expectedBracket = expectedBrackets[expectedBrackets.length - 1];
+
+    if (isOpeningBracket) {
+      if (char === expectedBracket) expectedBrackets.splice(-1, 1);
+      else expectedBrackets.push(closingBrackets[openingBrackets.indexOf(char)]);
+      continue;
+    }
+
+    if (isClosingBracket) {
+      if (char !== expectedBracket) return false;
+      expectedBrackets.splice(-1, 1);
+    }
+  }
+
+  return (expectedBrackets.length === 0 ) ? true : false;
 }
